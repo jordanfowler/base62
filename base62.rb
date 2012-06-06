f15e8b81059d84d0526437aa6ff3692eb50ea2cb
Jordan Fowler <me@jordanfowler.com>
2012-06-06 04:04:51 -0700

    Class methods.

--- base62.rb
+++ base62.rb
@@ -1,46 +1,32 @@
-module Base62
+class Base62
   SIXTYTWO = ('0'..'9').to_a + ('a'..'z').to_a + ('A'..'Z').to_a
-  
-  def encode( numeric )
-    raise ArgumentError unless Numeric === numeric
 
-    return '0' if numeric == 0
-    s = ''
+  class << self
+    def encode( numeric )
+      raise ArgumentError unless Numeric === numeric
 
-    while numeric > 0
-      s << Base62::SIXTYTWO[numeric.modulo(62)]
-      numeric /= 62
-    end
-    s.reverse
-  end
+      return '0' if numeric == 0
+      s = ''
 
-  def decode( base62 )
-    s = base62.to_s.reverse.split('')
-    
-    total = 0
-    s.each_with_index do |char, index|
-      if ord = SIXTYTWO.index(char)
-        total += ord * (62 ** index)
-      else
-        raise ArgumentError, "#{base62} has #{char} which is not valid"
+      while numeric > 0
+        s << Base62::SIXTYTWO[numeric.modulo(62)]
+        numeric /= 62
       end
-    end
-    total.to_s
-  end
-
-  module_function :encode, :decode
-end
-
-if $0 == __FILE__
-  require 'test/unit'
-  
-  class Base62Test < Test::Unit::TestCase
-    def test_base_62_of_max_big_int
-      assert_equal 'lYGhA16ahyf', Base62.encode(18446744073709551615)
+      s.reverse
     end
 
-    def test_numeric_of_base62d_max
-      assert_equal '18446744073709551615', Base62.decode('lYGhA16ahyf')
+    def decode( base62 )
+      s = base62.to_s.reverse.split('')
+
+      total = 0
+      s.each_with_index do |char, index|
+        if ord = SIXTYTWO.index(char)
+          total += ord * (62 ** index)
+        else
+          raise ArgumentError, "#{base62} has #{char} which is not valid"
+        end
+      end
+      total.to_s
     end
   end
 end
